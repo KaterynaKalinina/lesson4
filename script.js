cd26fd33b25f812ca53709956743f9e5e6e57e5b
Kateryna Kalinina <katya18chr@ukr.net>
2020-07-02 21:00:18 +0300

    Lesson4

---
+++ script.js
@@ -0,0 +1,22 @@
+/*let userIsHappy = true;
+let number = 0;
+
+do {
+     userIsHappy = confirm('Are you happy that the number is ' + ++number + '?');
+} while (userIsHappy);
+
+
+let box = document.querySelector('.box');
+let moveBy = 200;
+let moved = parseInt(box.style.marginLeft) || 0;
+
+do {
+    box.style.marginLeft = ++moved + 'px';
+} while (moved < moveBy);
+
+
+for (let number = 1, isUserHappy = true; isUserHappy; number++) {
+    isUserHappy = confirm('Are you happy that the number is ' + number + '?');
+}
+*/
+
